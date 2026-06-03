6416873446de1215b1fe71cfbf838fbdd7f428df
farheenNisha <farheennisha1405@gmail.com>
2026-06-03 18:12:45 +0530

    added more methods in linked list

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -158,6 +158,10 @@ public Node remove(int index) {
 
     return temp;
 }
+//find middle node 
+public Node removeMiddle(){
+
+}
 
 //reverse the list 
 public void reverse() {
@@ -175,6 +179,193 @@ public void reverse() {
         before = temp;
         temp = after;
     }
+}
+
+// has loop 
+public boolean hasLoop() {
+
+    Node slow = head;
+    Node fast = head;
+
+    while (fast != null && fast.next != null) {
+
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+// find the kth node from end 
+public Node findKthFromEnd(int k) {
+
+    if (k <= 0) {
+        return null;
+    }
+
+    Node slow = head;
+    Node fast = head;
+
+    for (int i = 0; i < k; i++) {
+        if (fast == null) {
+            return null;
+        }
+        fast = fast.next;
+    }
+
+    while (fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
+
+import java.util.HashSet;
+
+public void removeDuplicates() {
+
+    HashSet<Integer> set = new HashSet<>();
+
+    Node current = head;
+    Node previous = null;
+
+    while (current != null) {
+
+        if (set.contains(current.value)) {
+            previous.next = current.next;
+        } else {
+            set.add(current.value);
+            previous = current;
+        }
+
+        current = current.next;
+    }
+}
+public int binaryToDecimal() {
+
+    int num = 0;
+    Node current = head;
+
+    while (current != null) {
+        num = num * 2 + current.value;
+        current = current.next;
+    }
+
+    return num;
+}
+//paritition element 
+public void partitionList(int x) {
+
+    if (head == null) {
+        return;
+    }
+
+    Node dummy1 = new Node(0);
+    Node dummy2 = new Node(0);
+
+    Node prev1 = dummy1;
+    Node prev2 = dummy2;
+
+    Node current = head;
+
+    while (current != null) {
+
+        if (current.value < x) {
+            prev1.next = current;
+            prev1 = current;
+        } else {
+            prev2.next = current;
+            prev2 = current;
+        }
+
+        current = current.next;
+    }
+
+    prev2.next = null;
+    prev1.next = dummy2.next;
+
+    head = dummy1.next;
+}
+//reversing part of a list 
+public void reverseBetween(int m, int n) {
+
+    if (head == null || m == n) {
+        return;
+    }
+
+    Node dummy = new Node(0);
+    dummy.next = head;
+
+    Node prev = dummy;
+
+    for (int i = 0; i < m; i++) {
+        prev = prev.next;
+    }
+
+    Node current = prev.next;
+
+    for (int i = 0; i < n - m; i++) {
+
+        Node temp = current.next;
+
+        current.next = temp.next;
+
+        temp.next = prev.next;
+
+        prev.next = temp;
+    }
+
+    head = dummy.next;
+}
+public void swapPairs() {
+
+    Node dummy = new Node(0);
+    dummy.next = head;
+
+    Node prev = dummy;
+
+    while (prev.next != null && prev.next.next != null) {
+
+        Node first = prev.next;
+        Node second = first.next;
+
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+    }
+
+    head = dummy.next;
+}
+
+//swap nodes
+public void swapPairs() {
+
+    Node dummy = new Node(0);
+    dummy.next = head;
+
+    Node prev = dummy;
+
+    while (prev.next != null && prev.next.next != null) {
+
+        Node first = prev.next;
+        Node second = first.next;
+
+        // Swap the pair
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        // Move to the next pair
+        prev = first;
+    }
+
+    head = dummy.next;
 }
     // Main method for testing
     public static void main(String[] args) {
@@ -194,7 +385,7 @@ public void reverse() {
 
         // Test get and set methods
         System.out.println("Value returned by get method: " + myLL.get(2).value);
-        myLL.set(1, 4);
+        //myLL.set(1, 4);
 
         // Print the linked list
         myLL.printLL();
